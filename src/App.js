f15f4d9d034441e2ff7c273dac2c90c4bac336de
albinkafexhiu <kafexhiu.22@gmail.com>
2024-04-23 22:04:01 +0200

    Updated project structure and components

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import Form from "./Components/Form";
+import Header from "./Components/Header";
+import React, { useState } from "react";
+import Table from "./Components/Table";
+function App() {
+  const [userInput, setUserInput] = useState(null);
+
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
+  };
+
+  const yearlyData = [];
+
+  if (userInput) {
+    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
+    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
+    const expectedReturn = +userInput["expected-return"] / 100;
+    const duration = +userInput["duration"];
+
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  }
+  return (
+    <div>
+      <Header />
+      <Form onCalculate={calculateHandler}></Form>
+      {!userInput && <p style={{textAlign: 'center'}}>No investment calculated yet.</p>}
+      {userInput && 
+        <Table
+          data={yearlyData}
+          initialInvestment={userInput["current-savings"]}
+        />
+      }
+
+      {/* Todo: Show below table conditionally (only once result data is available) */}
+      {/* Show fallback text if no data is available */}
+    </div>
+  );
+}
+
+export default App;
